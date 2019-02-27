dc95feba459c8903456f0cd69476c9eee9de2256
i54988 <DKEllis@Xactware.com>
2019-02-27 00:01:54 -0700

    start over

--- bash/delete-old-files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# setup cronjob to run this script periodically
-# deletes all files and folders in specified directories after "-mtime -n" number of days.
-find /path/to/dir/with/files/to/watch -maxdepth 1 -mtime +7 -exec rm -R {} \;
-find /other/path/to/dir/with/file/to/watch -maxdepth 1 -mtime +7 -exec rm -R {} \;
--- bash/unrar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# run with a cronjob
-
-# will scan directories recursively for rar files, and unrar them in the same directory.
-# will skip if rar has already been extracted.
-find /path/to/dir/with/rar -name '*.rar' -execdir unrar e -o- {} \;
-find /path/to/other/dir/with/rar -name '*.rar' -execdir unrar e -o- {} \;
